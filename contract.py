a21a655531ec786822804d6b1cf8214f90e07b40
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-11-21 13:06:20 -0500

    Beginner Files

---
+++ contract.py
@@ -0,0 +1,15 @@
+
+
+
+class Contract:
+    wave = 0
+    
+    def __init__(self):
+        print("This is a sample contract")
+
+    def add_wave(self):
+        self.wave += 1
+
+    
+    
+    
